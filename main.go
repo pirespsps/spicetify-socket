ed08f5dab0bf336eef72b80c5bb0eab8359a81f4
pirespsps <matheusknoll28@gmail.com>
2025-11-29 11:53:11 -0300

    server e client socket conectando

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,12 +9,26 @@ import (
 
 func main() {
 
-	http.HandleFunc("/ws", WsHandler)
+	clientSocket()
+
+}
+
+func startServer() {
+	http.HandleFunc("/ws", ServerSocket)
 	fmt.Printf("Websocket server started on :8080")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func clientSocket() {
+
+	option := flag.String("option", "current", "select an option from [skip,previous,play,current]")
+
+	flag.Parse()
+
+	ClientSocket(*option)
 
 }
--- websocket.go
+++ websocket.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/websocket"
 )
@@ -13,7 +15,7 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func WsHandler(w http.ResponseWriter, r *http.Request) {
+func ServerSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Print("wsHandler error")
@@ -23,14 +25,38 @@ func WsHandler(w http.ResponseWriter, r *http.Request) {
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
-			fmt.Print("message error")
+			fmt.Print("message error: ", err)
 			break
 		}
 		fmt.Printf("Received: %s\\n", message)
 
+		//return the message
 		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
-			fmt.Print("message write error")
+			fmt.Print("message write error: ", err)
 			break
 		}
 	}
+}
+
+func ClientSocket(option string) {
+	u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/ws"}
+	fmt.Print("connected to", u)
+
+	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("connection error: ", err)
+	}
+	defer conn.Close()
+
+	err = conn.WriteMessage(websocket.TextMessage, []byte(option))
+	if err != nil {
+		log.Fatal("write message error: ", err)
+	}
+
+	_, message, err := conn.ReadMessage()
+	if err != nil {
+		log.Fatal("connection message received error: ", err)
+	}
+
+	fmt.Print("message: ", message)
 }
